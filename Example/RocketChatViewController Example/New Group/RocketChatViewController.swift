f5c8e974a3d0607ff6dd8b69203005184d725210
Rafael Kellermann Streit <rafaelks@me.com>
2018-07-30 17:34:54 -0300

    Create a new view controller & set it as the RocketChatViewController

---
+++ Example/RocketChatViewController Example/New Group/RocketChatViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RocketChatViewController.swift
+//  RocketChatViewController Example
+//
+//  Created by Rafael Kellermann Streit on 30/07/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+final class RocketChatViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var viewComposer: UIView!
+
+}
