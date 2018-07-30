7ae7f79a0c7e2802b2b60b8d0fa82a9f85993c72
Rafael Kellermann Streit <rafaelks@me.com>
2018-07-30 17:26:49 -0300

    Add more info to the gitignore

---
+++ RocketChatViewController Example/RocketChatViewController Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RocketChatViewController Example
+//
+//  Created by Rafael Kellermann Streit on 30/07/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
