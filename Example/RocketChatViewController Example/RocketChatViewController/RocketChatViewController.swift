531a594d3c5e7d6db3bae3064e33f6a192c347f6
filipealva <ofilipealvarenga@gmail.com>
2018-08-05 20:24:35 -0300

    Implement a basic list based on the proposed data modelling

--- Example/RocketChatViewController Example/RocketChatViewController/RocketChatViewController.swift
+++ Example/RocketChatViewController Example/RocketChatViewController/RocketChatViewController.swift
@@ -25,11 +25,49 @@ final class RocketChatViewController: UIViewController {
     @IBOutlet weak var viewComposer: UIView!
 
     let dataController = DataController()
+    var data: [ChatData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataController.data = DataControllerPlaceholder.generateDumbData(elements: 5000)
-        
+
+        collectionView.register(
+            UINib(
+                nibName: BasicMessageCollectionViewCell.identifier, bundle: nil
+            ),
+            forCellWithReuseIdentifier: BasicMessageCollectionViewCell.identifier
+        )
+
+        data = DataControllerPlaceholder.generateDumbData(elements: 5000)
     }
 
+}
+
+extension RocketChatViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return data.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data[section].sectionController.viewModels().count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let sectionController = data[indexPath.section].sectionController
+        let viewModel = sectionController.viewModels()[indexPath.row]
+
+        return sectionController.cell(for: viewModel, on: collectionView, at: indexPath)
+    }
+}
+
+extension RocketChatViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sectionController = data[indexPath.section].sectionController
+        let viewModel = sectionController.viewModels()[indexPath.row]
+
+        guard let height = sectionController.height(for: viewModel) else {
+            return CGSize.zero
+        }
+
+        return CGSize(width: UIScreen.main.bounds.width, height: height)
+    }
 }
