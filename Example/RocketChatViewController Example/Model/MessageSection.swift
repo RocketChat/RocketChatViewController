a995b13350244dffd7f6c1bd14424abbd1636674
filipealva <ofilipealvarenga@gmail.com>
2018-08-06 17:28:09 -0300

    Change IGListKit/Diffing for DifferenceKit

---
+++ Example/RocketChatViewController Example/Model/MessageSection.swift
@@ -0,0 +1,9 @@
+//
+//  MessageSection.swift
+//  RocketChatViewController Example
+//
+//  Created by Filipe Alvarenga on 06/08/18.
+//  Copyright © 2018 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
