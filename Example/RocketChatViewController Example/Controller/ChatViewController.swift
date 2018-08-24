36ff8e09641273a730ca5292783aea555e728755
filipealva <ofilipealvarenga@gmail.com>
2018-08-24 16:49:51 -0300

    Add ChatViewController to example project since RocketChatViewController is supposed to be a superclass

--- Example/RocketChatViewController Example/Controller/ChatViewController.swift
+++ Example/RocketChatViewController Example/Controller/ChatViewController.swift
@@ -7,29 +7,23 @@
 //
 
 import UIKit
+import DifferenceKit
 
-class ChatViewController: UIViewController {
+final class ChatViewController: RocketChatViewController {
+    let dataController = DataController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.register(
+            UINib(
+                nibName: BasicMessageCollectionViewCell.identifier, bundle: nil
+            ),
+            forCellWithReuseIdentifier: BasicMessageCollectionViewCell.identifier
+        )
+
+        data = DataControllerPlaceholder.generateDumbData(elements: 2)
+        data = []
+        data = DataControllerPlaceholder.generateDumbData(elements: 5000)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
