1837d4c74b18592d29424528d7537892c2c659e3
filipealva <ofilipealvarenga@gmail.com>
2018-08-24 17:16:44 -0300

    Remove unused file

--- Example/RocketChatViewController Example/Model/MessageSection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MessageSection.swift
-//  RocketChatViewController Example
-//
-//  Created by Filipe Alvarenga on 06/08/18.
-//  Copyright © 2018 Rocket.Chat. All rights reserved.
-//
-
-import Foundation
